c80b2524f75ac003c0875db91c6e8485fd2c71d0
Matthew Dunbar <matthewdunbar11@gmail.com>
2018-01-06 13:13:32 -0500

    Add custom network manager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnServerConnect(NetworkConnection conn) {
+		GameObject.Find ("Game Status").gameObject.GetComponent<Text>().text = "Game Starting";
+	}
+}
