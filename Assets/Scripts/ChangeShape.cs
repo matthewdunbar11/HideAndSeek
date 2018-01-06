b3348c789138a7005baa7990e1541e94ebf38525
Matthew Dunbar <matthewdunbar@matthews-mbp.lan>
2018-01-06 00:25:29 -0500

    Switch player shape across the network

--- Assets/Scripts/ChangeShape.cs
+++ Assets/Scripts/ChangeShape.cs
@@ -1,29 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class ChangeShape : MonoBehaviour {
+public class ChangeShape : NetworkBehaviour {
 
     int currentGameObject = 0;
     int totalGameObjects;
 	// Use this for initialization
 	void Start () {
-        totalGameObjects = transform.GetChildCount();
+        totalGameObjects = transform.childCount;
     }
 	
 	// Update is called once per frame
 	void Update () {
+		if (!isLocalPlayer)
+			return;
+		
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            transform.GetChild(currentGameObject).gameObject.SetActive(false);
             currentGameObject++;
             if(currentGameObject >= totalGameObjects)
             {
                 currentGameObject = 0;
             }
 
-            transform.GetChild(currentGameObject).gameObject.SetActive(true);
+			CmdSwitchVisibleChild (currentGameObject);
+//			RpcSwitchVisibleChild (currentGameObject);
+			SwitchVisibleChild (currentGameObject);
         }
 
     }
+
+	[Command]
+	void CmdSwitchVisibleChild(int index) {
+		RpcSwitchVisibleChild (index);
+	}
+
+	[ClientRpc]
+	void RpcSwitchVisibleChild(int index) {
+		SwitchVisibleChild (index);
+	}
+
+	void SwitchVisibleChild(int index) {
+		for (var i = 0; i < transform.childCount; i++) {
+			transform.GetChild (i).gameObject.SetActive (false);
+		}
+		transform.GetChild (index).gameObject.SetActive (true);		
+	}
 }
